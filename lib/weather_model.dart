ab575dc57216cf600ef2a53f8afdc754dbd0fc94
Faheem Ahmad <fisforfaheem@gmail.com>
2023-08-14 11:28:22 +0500

    Added .aider* to .gitignore and created a Weather class with its properties and a constructor that initializes them from a JSON object.

---
+++ lib/weather_model.dart
@@ -0,0 +1,43 @@
+class Weather {
+  Weather(
+      {required this.cityName,
+      required this.icon,
+      required this.condition,
+      required this.temp,
+      required this.wind,
+      required this.humidity,
+      required this.windDir,
+      required this.gust,
+      required this.uv,
+      required this.pressure,
+      required this.pricipe,
+      required this.lastUpdate});
+
+  Weather.fromJson(Map<String, dynamic> json) {
+    cityName = json['location']['name'];
+    icon = json['current']['condition']['icon'];
+    condition = json['current']['condition']['text'];
+    temp = json['current']['temp_c'];
+    wind = json['current']['wind_kph'];
+    humidity = json['current']['humidity'];
+    windDir = json['current']['wind_dir'];
+    gust = json['current']['gust_kph'];
+    uv = json['current']['uv'];
+    pressure = json['current']['pressure_mb'];
+    pricipe = json['current']['precip_mm'];
+    lastUpdate = json['current']['last_updated'];
+  }
+
+  var cityName;
+  var condition;
+  var gust;
+  var humidity;
+  var icon;
+  var lastUpdate;
+  var pressure;
+  var pricipe;
+  var temp;
+  var uv;
+  var wind;
+  var windDir;
+}
